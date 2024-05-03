e7dce24bb40306df98c7432b9c44f38fe1617740
southPaw <dangargiullo@gmail.com>
2024-05-03 10:53:03 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+// Source: https://tailwindcss.com/docs/guides/vite -> tailwind.config.js
+// https://youtu.be/LDB4uaJ87e0 @26:48 - 30:24
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Roboto', 'sans-serif']
+            },
+            gridTemplateColumns: {
+                '70/30': '70% 28%',
+            }
+        },
+    },
+    plugins: [],
+}
