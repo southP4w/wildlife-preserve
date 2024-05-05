ced17abcfddc16f373e537074ef6073b3529fc19
southPaw <dangargiullo@gmail.com>
2024-05-04 21:10:22 -0400

    Add `passes.json`, need to fix: Page not rendering when calling `<PassListings/>` from `App.jsx`

--- src/components/PassListings.jsx
+++ src/components/PassListings.jsx
@@ -1,80 +1,36 @@
 import passes from '../passes.json'
 
 export const PassListings = () => {
-    console.log(passes)
     return (
         <section className='bg-blue-50 px-4 py-10'>
             <div className='container-xl lg:container m-auto'>
                 <h2 className='text-3xl font-bold text-indigo-500 mb-6 text-center'>
                     h2 </h2>
                 <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-                    {/* Source: https://youtu.be/LDB4uaJ87e0?t=3529 */}
-                    {/**/}
-                    <div className='bg-white rounded-xl shadow-md relative'>
-                        <div className='p-4'>
-                            <div className='mb-6'>
-                                <div className='text-gray-600 my-2'>div: className=text-gray-600 my-2</div>
-                                <h3 className='text-xl font-bold'>h3: className=text-xl font-bold</h3>
-                            </div>
-                            <div className='mb-5'>
-                                div: className=mb-5
-                            </div>
-                            <h3 className='text-indigo-500 mb-2'>h3: className=text-indigo-500 mb-2</h3>
-                            <div className='border border-gray-100 mb-5'></div>
-                            <div className='flex flex-col lg:flex-row justify-between mb-4'>
-                                <div className='text-orange-700 mb-3'>
-                                    <i className='fa-solid fa-location-dot text-lg'></i>
-                                    div: className=text-orange-700 mb-3
+                    {passes.map((pass) => (
+                        <div key={pass.id} className='bg-white rounded-xl shadow-md relative'>
+                            <div className='p-4'>
+                                <div className='mb-6'>
+                                    <div className='text-gray-600 my-2'>{pass.description}</div>
+                                    <h3 className='text-xl font-bold'></h3>
                                 </div>
-                                <a href='page.html' className='h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm'>
-                                    Read More
-                                </a>
+                                <div className='mb-5'>
+                                    div: className=mb-5
+                                </div>
+                                <h3 className='text-indigo-500 mb-2'>h3: className=text-indigo-500 mb-2</h3>
+                                <div className='border border-gray-100 mb-5'></div>
+                                <div className='flex flex-col lg:flex-row justify-between mb-4'>
+                                    <div className='text-orange-700 mb-3'>
+                                        <i className='fa-solid fa-location-dot text-lg'></i>
+                                        div: className=text-orange-700 mb-3
+                                    </div>
+                                    <a href='page.html' className='h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm'>
+                                        Read More
+                                    </a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className='bg-white rounded-xl shadow-md relative'>
-                        <div className='p-4'>
-                            <div className='mb-6'>
-                                <div className='text-gray-600 my-2'>div: className=text-gray-600 my-2</div>
-                                <h3 className='text-xl font-bold'>h3: className=text-xl font-bold</h3>
-                            </div>
-                            <div className='mb-5'>
-                                div: className=mb-5
-                            </div>
-                            <h3 className='text-indigo-500 mb-2'>h3: className=text-indigo-500 mb-2</h3>
-                            <div className='border border-gray-100 mb-5'></div>
-                            <div className='flex flex-col lg:flex-row justify-between mb-4'>
-                                <div className='text-orange-700 mb-3'>
-                                    <i className='fa-solid fa-location-dot text-lg'></i>
-                                    div: className=text-orange-700 mb-3
-                                </div>
-                                <a href='page.html' className='h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm'>Read More
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='bg-white rounded-xl shadow-md relative'>
-                        <div className='p-4'>
-                            <div className='mb-6'>
-                                <div className='text-gray-600 my-2'>div: className=text-gray-600 my-2</div>
-                                <h3 className='text-xl font-bold'>h3: className=text-xl font-bold</h3>
-                            </div>
-                            <div className='mb-5'>
-                                div: className=mb-5
-                            </div>
-                            <h3 className='text-indigo-500 mb-2'>h3: className=text-indigo-500 mb-2</h3>
-                            <div className='border border-gray-100 mb-5'></div>
-                            <div className='flex flex-col lg:flex-row justify-between mb-4'>
-                                <div className='text-orange-700 mb-3'>
-                                    <i className='fa-solid fa-location-dot text-lg'></i>
-                                    div: className=text-orange-700 mb-3
-                                </div>
-                                <a href='page.html' className='h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm'>
-                                    Read More
-                                </a>
-                            </div>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
         </section>
