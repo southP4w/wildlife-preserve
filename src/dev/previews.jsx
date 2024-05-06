21a5eb26a1dc6dd6416124bea8e9eea73a4f8fd6
southPaw <dangargiullo@gmail.com>
2024-05-05 22:21:22 -0400

    Add `dev` directory

---
+++ src/dev/previews.jsx
@@ -0,0 +1,15 @@
+import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox'
+import {PaletteTree} from './palette'
+import {PassListings} from "../components/PassListings.jsx";
+
+const ComponentPreviews = () => {
+    return (
+        <Previews palette={<PaletteTree/>}>
+            <ComponentPreview path='/PassListings'>
+                <PassListings/>
+            </ComponentPreview>
+        </Previews>
+    )
+}
+
+export default ComponentPreviews
