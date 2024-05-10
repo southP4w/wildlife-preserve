0ebdd51189efc749448f3556ebd0cbc8b37b8406
southPaw <dangargiullo@gmail.com>
2024-05-09 20:50:00 -0400

    Add expanding/collapsing description section to `PassListing.jsx`

--- src/components/PassListing.jsx
+++ src/components/PassListing.jsx
@@ -1,4 +1,11 @@
+import {useState} from 'react'
+import {FaMapMarker} from 'react-icons/fa'
+
 export const PassListing = ({pass}) => {
+	const [showDescription, setShowDescription] = useState(false)
+	let description = pass.description
+	if (!showDescription)
+		description = description.substring(0, 90) + '...'
 	return (
 		<div className='bg-white rounded-xl shadow-md relative'>
 			<div className='p-4'>
@@ -7,10 +14,12 @@ export const PassListing = ({pass}) => {
 					<h3 className='text-xl font-bold'></h3>
 				</div>
 				<div className='mb-5'></div>
-				<h3 className='text-indigo-500 mb-2'>{pass.description}</h3>
+				<h3 className='text-indigo-500 mb-2'>{description}</h3>
+				<button onClick={() => setShowDescription((previousState) => !previousState)} className={'text-indigo-500.mb-5 hover:text-indigo-600'}>Show {showDescription ? 'less' : 'more'}</button>
 				<div className='border border-gray-100 mb-5'></div>
 				<div className='flex flex-col lg:flex-row justify-between mb-4'>
 					<div className='text-orange-700 mb-3'>
+						<FaMapMarker className={'inline text-lg mb-1 mr-1'}/>
 						<i className='fa-solid fa-location-dot text-lg'></i>
 					</div>
 					<a href={`/pass/${pass.id}`} className='h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm'>
@@ -19,6 +28,5 @@ export const PassListing = ({pass}) => {
 				</div>
 			</div>
 		</div>
-
 	)
 }
