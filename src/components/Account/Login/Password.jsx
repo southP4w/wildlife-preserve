ce9a253ec98e4b4a0c1824ce3c29be77ce8b180a
Ardit Hoxhaj <ardit.hoxhaj00@qmail.cuny.edu>
2024-05-12 15:29:15 -0400

    Added Udemy project AutoText.jsx and re-arranged Login.jsx

--- src/components/Account/Login/Password.jsx
+++ src/components/Account/Login/Password.jsx
@@ -1,58 +1,47 @@
 import { NavLink } from 'react-router-dom';
-import background from '../../../assets/images/ZooBack.jpg';
 
 export const Password = () => {
 	return (
-		<section style={{
-			backgroundImage: `url(${background})`,
-			backgroundSize: 'cover',
-			backgroundPosition: 'center',
-			height: '100vh',
-			display: 'flex',
-			alignItems: 'center',
-			justifyContent: 'center'
-		}}>
-			<div style={{ width: '100%', maxWidth: '400px', backgroundColor: 'white', padding: '20px', borderRadius: '8px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
-				<form>
-					<div className='mb-4'>
-						<label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='username'>
-							Username
-						</label>
-						<input
-							className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-							id='username'
-							type='text'
-							placeholder='Username'
-						/>
-					</div>
-					<div className='mb-6'>
-						<label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='password'>
-							Password
-						</label>
-						<input
-							className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'
-							id='password'
-							type='password'
-							placeholder='******************'
-						/>
-					</div>
-					<div className='flex items-center justify-between'>
-						<button
-							className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
-							type='submit'
-						>
-							Sign In
-						</button>
-						<NavLink
-							to='/creation'
-							className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline inline-block text-center'
-							style={{ lineHeight: 'normal' }} // Adjust line height if needed
-						>
-							Create Account
-						</NavLink>
-					</div>
-				</form>
-			</div>
-		</section>
+		<div style={{ width: '100%', maxWidth: '400px', backgroundColor: 'white', padding: '20px', borderRadius: '8px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
+			<form>
+				<div className='mb-4'>
+					<label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='username'>
+						Username
+					</label>
+					<input
+						className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+						id='username'
+						type='text'
+						placeholder='Username'
+					/>
+				</div>
+				<div className='mb-6'>
+					<label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='password'>
+						Password
+					</label>
+					<input
+						className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'
+						id='password'
+						type='password'
+						placeholder='******************'
+					/>
+				</div>
+				<div className='flex items-center justify-between'>
+					<button
+						className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+						type='submit'
+					>
+						Sign In
+					</button>
+					<NavLink
+						to='/creation'
+						className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline inline-block text-center'
+						style={{ lineHeight: 'normal' }}
+					>
+						Create Account
+					</NavLink>
+				</div>
+			</form>
+		</div>
 	);
 };
